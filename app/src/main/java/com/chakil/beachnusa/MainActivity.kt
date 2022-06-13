d88480542099e9d8b6ee8f1e0f8357809bcef682
dodichakill <dodiherlangga91@gmail.com>
2022-06-13 10:27:23 +0700

    menambahkan navigationBottom (namun terdapat bug dari home ke halaman lain balik ke home tampilanya blank)

--- app/src/main/java/com/chakil/beachnusa/MainActivity.kt
+++ app/src/main/java/com/chakil/beachnusa/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ListView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -43,5 +48,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        //navigasi
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
